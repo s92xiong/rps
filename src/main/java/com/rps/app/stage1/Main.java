4ef6cd8c228ed19a9961283372b0595f30df84f7
Sheuh Xiong <sheuhxiong@gmail.com>
2023-04-20 10:52:14 -0400

    Add stage1 solution

---
+++ src/main/java/com/rps/app/stage1/Main.java
@@ -0,0 +1,58 @@
+package com.rps.app.stage1;
+import java.util.Scanner;
+
+enum Gesture {
+    ROCK("SCISSORS", "PAPER"),
+    PAPER("ROCK", "SCISSORS"),
+    SCISSORS("PAPER", "ROCK");
+
+    private final String winsAgainst;
+    private final String losesTo;
+    Gesture(String winsAgainst, String losesTo) {
+        this.winsAgainst = winsAgainst;
+        this.losesTo = losesTo;
+    }
+
+    public String getWinsAgainst() {
+        return winsAgainst;
+    }
+
+    public String getLosesTo() {
+        return losesTo;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String input = getInput();
+        String computedResult = computeDecision(input);
+        printResult(computedResult);
+    }
+
+    public static String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static String computeDecision(String s) {
+        String result;
+        switch (s) {
+            case "rock":
+                result = Gesture.ROCK.getLosesTo();
+                break;
+            case "scissors":
+                result = Gesture.SCISSORS.getLosesTo();
+                break;
+            case "paper":
+                result = Gesture.PAPER.getLosesTo();
+                break;
+            default:
+                result = "";
+        }
+        return result.toLowerCase();
+    }
+
+    public static void printResult(String s) {
+        System.out.printf("Sorry, but the computer chose %s", s);
+    }
+}
